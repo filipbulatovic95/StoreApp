9b9f9b7cd8304c86cec515e3eb7a4284a66851d0
filip.bulatovic <filip.bulatovic95@gmail.com>
2023-09-14 14:55:24 +0200

    Section 5 vol.1 lession 53 ended

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
         public ProductsController(StoreContext context)
@@ -15,7 +13,7 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpGet("getAll")]
+        [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
             return await _context.Products.ToListAsync();
@@ -24,7 +22,11 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null) return NotFound();
+
+            return product;
         }
     }
 }
