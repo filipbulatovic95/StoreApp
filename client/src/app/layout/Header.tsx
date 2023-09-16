b3b004082d5a710de61c835754c90a8e528008b5
filip.bulatovic <filip.bulatovic95@gmail.com>
2023-09-16 15:53:07 +0200

    Section 6 completed

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Switch, Toolbar, Typography, List, ListItem, IconButton, Badge, Box } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useStoreContext } from "../context/StoreContext";
 
 interface Props {
     darkMode: boolean;
@@ -33,6 +34,10 @@ const navStyles = {
 
 
 export default function Header({darkMode, handleThemeChange} : Props) {
+    const {basket} = useStoreContext();
+    const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
+
+
     return (
       <AppBar position="static" sx={{ mb: 4 }}>
         <Toolbar
@@ -63,13 +68,13 @@ export default function Header({darkMode, handleThemeChange} : Props) {
           </List>
 
           <Box display='flex' alignItems='center'>
-            <IconButton
+            <IconButton component={Link} to="/basket"
               size="large"
               edge="start"
               color="inherit"
               sx={{ mr: 2 }}
             >
-              <Badge badgeContent="4" color="secondary">
+              <Badge badgeContent={itemCount} color="secondary">
                 <ShoppingCart />
               </Badge>
             </IconButton>
