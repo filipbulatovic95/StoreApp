8bcf31b2aef8517e2f1a3585381bc76bd54f61c3
filip.bulatovic <filip.bulatovic95@gmail.com>
2023-09-21 13:47:27 +0200

    Section 8 completed

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,23 +2,88 @@ import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
 import ProductList from "./ProductList";
 import { useEffect } from "react";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors, setPageNumber, setProductParams } from "./catalogSlice";
+import { Grid, Paper } from "@mui/material";
+import ProductSearch from "./ProductSearch";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
+import CheckboxButtons from "../../app/components/CheckboxButtons";
+import AppPagination from "../../app/components/AppPagination";
+
+const sortOptions = [
+  {value: 'name', label: 'Alphabetical'},
+  {value: 'priceDesc', label: 'Price - High to low'},
+  {value: 'price', label: 'price - Low to high'},
+]
 
 
 export default function Catalog(){
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const {productsLoaded, filtersLoaded, brands, types, productParams, metaData} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
       if(!productsLoaded) dispatch(fetchProductsAsync());
     }, [productsLoaded, dispatch]); //dependency [] znaci da ce ovo biti pozvano samo jednom
 
-    if(status.includes('pending')) return <LoadingComponent message='Loading products...'/>
+    useEffect(() => {
+      if(!filtersLoaded) dispatch(fetchFilters());
+    }, [dispatch, filtersLoaded]); 
+    //2 useEffecta pravimo da ne bi api loadovao 2 puta products
+    //posto ako stavimo sve u jedan use effect i 
+    //stavimo dependency da ima i productsLoaded i filtersLoaded
+    //kad se loaduju products, productsLoaded ce da bude true
+    //onda kada se loaduju filteri filtersLoaded ce biti true
+    //posto je true opet ce da se loaduje, nece filteri ali hoce products
+
+    if(!filtersLoaded) return <LoadingComponent message='Loading products...'/>
 
     return (
-        <>
-            <ProductList products={products}/>
-      </>
-    )
+      <Grid container columnSpacing={4}>
+        <Grid item xs={3}>
+          <Paper sx={{ mb: 2 }}>
+            <ProductSearch />
+          </Paper>
+          <Paper sx={{ mb: 2, p: 2 }}>
+            <RadioButtonGroup
+              selectedValue={productParams.orderBy}
+              options={sortOptions}
+              onChange={(e) =>
+                dispatch(setProductParams({ orderBy: e.target.value }))
+              }
+            />
+          </Paper>
+
+          <Paper sx={{ mb: 2, p: 2 }}>
+            <CheckboxButtons
+              items={brands}
+              checked={productParams.brands}
+              onChange={(items: string[]) =>
+                dispatch(setProductParams({ brands: items }))
+              }
+            />
+          </Paper>
+
+          <Paper sx={{ mb: 2, p: 2 }}>
+            <CheckboxButtons
+              items={types}
+              checked={productParams.types}
+              onChange={(items: string[]) =>
+                dispatch(setProductParams({ types: items }))
+              }
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={9}>
+          <ProductList products={products} />
+        </Grid>
+        <Grid item xs={3} />
+        <Grid item xs={9} sx={{mb: 2}}>
+          {metaData &&
+          <AppPagination 
+            metaData={metaData} //if(status.includes('pending') || !metaData) return <LoadingComponent message='Loading products...'/> ovde je dodat !metaData posto je error da metaData cannot be null
+            onPageChange={(page: number) => dispatch(setPageNumber({pageNumber: page}))}
+          />}
+        </Grid>
+      </Grid>
+    );
 }
